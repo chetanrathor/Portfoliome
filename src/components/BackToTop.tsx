ad3a50a2199836b297e449f8118a3da06975e02f
Chetan Rathore <crathore@grepruby.io>
2024-11-23 21:08:38 +0530

    triggered deploy

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -9,7 +9,7 @@ const BackToTop = () => {
             window.scrollTo({behavior:'smooth',top:0,})
         }} container justifyContent={'center'}  alignItems={'center'} sx={{
             width: '50px',
-            height: '50px',
+            height: '50px', 
             borderRadius: '50%',
             backgroundColor: theme.palette.primary.main,
             margin:'10px'
